b40901d554c9d5c23eeb6322070c8de73d8bcdb1
arknave <arnavsastry@gmail.com>
2014-08-18 23:29:40 -0500

    SPOJ Samer08f, also known as "Feynman". stupid input errors

---
+++ spoj/samer08f.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int dp[101];
+    dp[1] = 1;
+    for(int i=2;i<=100;i++) {
+        dp[i] = 1;
+        for(int j=i-1, sq=2;j>0;j--,sq++) {
+            dp[i] += sq*sq;
+        }
+    }
+    int N = 1;
+    cin >> N;
+    while (N != 0) {
+        cout << dp[N] << endl;
+        cin >> N;
+    }
+}
