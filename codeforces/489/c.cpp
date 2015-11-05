339317d18d99909166b8da3bf1e303d3c3725033
Arnav Sastry <arnavsastry@gmail.com>
2015-11-05 15:31:01 -0600

    updated old problem

--- codeforces/489/c.cpp
+++ codeforces/489/c.cpp
@@ -13,29 +13,28 @@ char min_val[101];
 char max_val[101];
 
 void find_min(int M, int S) {
+    if (S == 0) {
+        min_val[0] = '0';
+        return;
+    }
+
     int sum = S;
-    for (int i = 0; i < M; i++) {
-//        cout << M << " " << S << '\n';
-        if (sum == 0) {
-            min_val[i] = '0'; 
+    for (int i = 0; i < M; ++i) {
+        int chars_left = M - i - 1;
+        int sum_left = 9 * chars_left;
+
+        if (sum <= sum_left) {
+            char nextChar = (i == 0) ? '1' : '0';
+            min_val[i] = nextChar;
+            sum -= (nextChar - '0');
         } else {
-            int digits_left = M - i - 1;
-            int max_sum_left = digits_left * 9;
-//            cout << "digits_left: " << digits_left << '\n';
-//            cout << "max_sum_left: " << max_sum_left << '\n';
-            if (sum <= max_sum_left) {
-                min_val[i] = (i == 0 ? '1' : '0');
-                sum -= (i == 0 ? 1 : 0);
+            int diff = sum - sum_left;
+            if (diff >= 10) {
+                min_val[0] = '0';
+                break;
             } else {
-                int diff = sum - max_sum_left;
-//                cout << "diff: " << diff << '\n';
-                if (diff > 9) {
-                    min_val[0] = '0';
-                    return;
-                } else {
-                    min_val[i] = '0' + diff;
-                    sum -= diff;
-                }
+                min_val[i] = '0' + diff;
+                sum -= diff;
             }
         }
     }
@@ -65,6 +64,7 @@ int main() {
     } else {
         find_min(M, S);
         find_max(M, S);
+
         if (min_val[0] == '0' || max_val[0] == '0') {
             cout << "-1 -1";
         } else {
@@ -78,5 +78,6 @@ int main() {
         }
         cout << '\n';
     }
+
     return 0;
 }
