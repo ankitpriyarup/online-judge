f41f41d2e1fcde6275aeac912bc48869c167d38a
Arnav Sastry <arnavsastry@gmail.com>
2017-03-20 10:31:01 -0500

    upsolve rmq

--- hackerrank/w30/rmq.cpp
+++ hackerrank/w30/rmq.cpp
@@ -1,9 +1,10 @@
 #include <cstdio>
+#include <vector>
 
 using namespace std;
 
 const int MAXN = 40002;
-const int BLOCK_SIZE = 401;
+const int BLOCK_SIZE = 301;
 
 int n;
 int a[MAXN];
@@ -11,8 +12,8 @@ int a[MAXN];
 // Modulo i, in bucket j, how many values are equal to k?
 int bucket_freq[BLOCK_SIZE][BLOCK_SIZE][BLOCK_SIZE];
 
-// Overall, how many values in bucket i are equal to j
-int big_bucket_freq[BLOCK_SIZE][MAXN];
+// List of occurences of i
+vector<int> freq[MAXN];
 
 // O(max(b, n / b))
 int small_query(const int& l, const int& r, const int& x, const int& y) {
@@ -31,29 +32,10 @@ int small_query(const int& l, const int& r, const int& x, const int& y) {
     return ans;
 }
 
-int equal(const int& l, const int& r, const int& v) {
-    int i = l + 1;
-    int ans = 0;
-
-    // O(max(b, n / b))
-    while (i <= r) {
-        if (i % BLOCK_SIZE == 0 and i + BLOCK_SIZE - 1 <= r) {
-            ans += big_bucket_freq[i / BLOCK_SIZE][v];
-            i += BLOCK_SIZE;
-        } else {
-            ans += (a[i] == v);
-            ++i;
-        }
-    }
-
-    return ans;
-}
-
 int big_query(const int& l, const int& r, const int& x, const int& y) {
     int ans = 0;
-    // O(n / b)
     for (int v = y; v < MAXN; v += x) {
-        ans += equal(l, r, v);
+        ans += upper_bound(freq[v].begin(), freq[v].end(), r) - upper_bound(freq[v].begin(), freq[v].end(), l);
     }
 
     return ans;
@@ -67,7 +49,7 @@ int main() {
     for (int i = 1; i <= n; ++i) {
         scanf("%d", a + i);
 
-        big_bucket_freq[i / BLOCK_SIZE][a[i]]++;
+        freq[a[i]].push_back(i);
         for (int j = 1; j < BLOCK_SIZE; ++j) {
             bucket_freq[j][i / BLOCK_SIZE][a[i] % j]++;
         }
